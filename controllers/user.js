358bc1c0494b481637f2817afd7800e8f92f70fb
iamatharvak <kulkarni.atharva.1403@gmail.com>
2024-12-24 09:01:27 +0530

    remaining commits

--- controllers/user.js
+++ controllers/user.js
@@ -7,9 +7,21 @@ async function handleUserSignup(req,res){
         email,
         password,
     });
-    return res.render("home");
+    return res.redirect("/");
+}
+
+async function handleUserLogin(req,res){
+    const {email,password} = req.body;
+    const user = await User.findOne ({ email,password,});
+    if(!user)
+
+    return res.render("login",{
+        error:"Invalid email or password"
+    });
+    return res.redirect("/");
 }
 
 module.exports = {
     handleUserSignup,
+    handleUserLogin,
 };
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -12,6 +12,9 @@ router.get('/', async (req,res)=>{
 router.get('/signup',(req,res)=>{
     return res.render('signup');
 });
+router.get('/login',(req,res)=>{
+    return res.render('login');
+});
 
 module.exports = router;
 
