c8f786caa2ca2acf4b1cbd2d23dc42635f60bbe1
iamatharvak <kulkarni.atharva.1403@gmail.com>
2024-12-28 06:05:48 +0530

    jwt

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 const { setUser } = require("../service/auth");
 
 async function handleUserSignup(req, res) {
@@ -19,9 +19,9 @@ async function handleUserLogin(req, res) {
     return res.render("login", {
       error: "Invalid email or password",
     });
-  const sessionId = uuidv4();
-  setUser(sessionId, user);
-  res.cookie("uid", sessionId);
+  // const sessionId = uuidv4();
+  const token = setUser(user);
+  res.cookie("uid", token);
   return res.redirect("/");
 };
 
--- service/auth.js
+++ service/auth.js
@@ -1,14 +1,33 @@
-const sessionIdtoUserMap = new Map();
+// const sessionIdtoUserMap = new Map();
 
-function setUser(id,user){
-    sessionIdtoUserMap.set(id,user);
-};
+const jwt = require("jsonwebtoken");
+const secret = "Atharva@123";
 
-function getUser(id){
-    return sessionIdtoUserMap.get(id);
-};
+function setUser(user) {
+  // const payload = {
+  //     id,
+  //     ...user,
+  // };
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
+}
+
+function getUser(token) {
+  // return sessionIdtoUserMap.get(id);
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
+}
 
 module.exports = {
-    setUser,
-    getUser,
+  setUser,
+  getUser,
 };
